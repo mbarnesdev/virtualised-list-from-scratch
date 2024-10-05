c14d822b414027201e8023485e238ad2f86b8ae9
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-05 22:29:43 +0100

    feat: stub out basic component api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import * as React from "react"
+import { List } from "./components/list"
+
+function ListRow() {
+  return <div></div>
+}
+
 function App() {
-  return <p>App</p>
+  const [data] = React.useState([])
+
+  return (
+    <List
+      height={150}
+      itemHeight={35}
+      itemCount={data.length}
+      data={data}
+    >
+      {ListRow}
+    </ List>
+  )
 }
 
 export default App
--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,7 +1,15 @@
 import * as React from "react"
 
-interface ListProps { }
+interface ListProps<T> {
+  height: number
+  itemHeight: number
+  itemCount: number
+  data: T[]
+  children: () => React.ReactNode
+}
 
-export const List: React.FC<ListProps> = () => {
+export function List<T>(props: ListProps<T>) {
+  console.log(props)
+
   return <div>List</div>
 }
